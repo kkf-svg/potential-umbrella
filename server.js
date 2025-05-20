2d4458c02c77707e935beaee4c7ccf0b569d1ade
kkf-svg <gopaulkirk@gmail.com>
2025-05-20 17:44:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
-// 1. Install dependencies: npm install express dotenv cors
-import express, { json }  from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import userRoutes from './routes/user.js';
 import contentRoutes from './routes/content.js';
@@ -8,13 +7,20 @@ import contentRoutes from './routes/content.js';
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(json());
 app.use(express.static('public'));
 app.use('/api', userRoutes);
 app.use('/api', contentRoutes);
 
+// error handling
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
 
-
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
