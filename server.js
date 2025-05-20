dd0782663dbe676c7bd678c098174b7c2ed786c3
kkf-svg <gopaulkirk@gmail.com>
2025-05-20 18:13:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-
 import express, { json } from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import userRoutes from './routes/user.js';
 import contentRoutes from './routes/content.js';
@@ -7,15 +7,22 @@ import contentRoutes from './routes/content.js';
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(json());
 app.use(express.static('public'));
 app.use('/api', userRoutes);
 app.use('/api', contentRoutes);
 
+// error handling
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
 
-
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log(`EduFlex Node server running on port ${PORT}`);
+  console.log(`EduFlex Node server running on port ${PORT}`);
 });
